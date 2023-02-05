0a101b04e93082514b5ca2a7719a94b9e149f21b
oluwasegun michael <alabioluwasegun8@gmail.com>
2023-02-05 23:52:22 +0100

    greetFunction

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 //function greetings
 
-function greet() {
-  console.log("Hello Baby");
-}
-greet();
+// function greet() {
+//   console.log("Hello Baby");
+// }
+// greet();
+
+(function (lastname) {
+  var firstname = "Michael";
+  console.log(firstname);
+  console.log(lastname);
+
+  console.log(`${firstname} ${lastname}`);
+})("Kings");
