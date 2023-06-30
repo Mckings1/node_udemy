aefb82c6cffb93fbfd7828a9b5b278b5491b7aa8
oluwasegun michael <alabioluwasegun8@gmail.com>
2023-06-30 13:39:27 +0100

    recentlyAdded

--- app.js
+++ app.js
@@ -5,10 +5,13 @@
 // }
 // greet();
 
-(function (lastname) {
-  var firstname = "Michael";
-  console.log(firstname);
-  console.log(lastname);
+// (function (lastname) {
+//   var firstname = "Michael";
+//   console.log(firstname);
+//   console.log(lastname);
 
-  console.log(`${firstname} ${lastname}`);
-})("Kings");
+//   console.log(`${firstname} ${lastname}`);
+//   console.log(`Hello, ${babyGirl}`);
+// })("Kings");
+
+var buffer = new ArrayBuffer(8);
