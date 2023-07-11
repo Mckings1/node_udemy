870711a53458ff22eb9657acb09d309863888ee8
oluwasegun michael <alabioluwasegun8@gmail.com>
2023-07-11 07:37:08 +0100

    jsCheckForsthInObj

--- app.js
+++ app.js
@@ -14,4 +14,62 @@
 //   console.log(`Hello, ${babyGirl}`);
 // })("Kings");
 
-var buffer = new ArrayBuffer(8);
+// var buffer = new ArrayBuffer(8);
+
+// function testEqual(val) {
+//   if (val == 12) {
+//     // Change this line
+//     return "Equal";
+//   }
+//   return "Not Equal";
+// }
+// console.log(testEqual(val));
+// // testEqual(10);
+
+// function testEqual() {
+//   3 === 3;
+//   3 == "3";
+
+//   console.log(testEqual);
+// }
+
+// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+  for (let i = 0; i < contacts.length; i++) {
+    if (contacts[i].firstName === name) {
+      return contacts[i][prop] || "No such property";
+    }
+  }
+  // Only change code above this line
+}
+
+var data = lookUpProfile("Akira", "likes");
+console.log(data);
